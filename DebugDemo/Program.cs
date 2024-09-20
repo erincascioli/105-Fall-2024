9b424f4cdc04c19e0feddd324d12e2784fc27440
erincascioli <edcigm@rit.edu>
2024-09-20 16:53:54 -0400

    Pushing if statement demo
    Contains multiple if statements for a number, along with if/elseif/elseif/else statements for a color choice.

--- DebugDemo/Program.cs
+++ DebugDemo/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Divide by Zero Exception
+            // Computer not "smart enough" to interpret the value of number as 0
+            //   and then warn the programmer that their code will malfunction.
+            // Passes all syntax tests, so VS sees it as good! 
+            //int number = 0;
+            //int result = 10 / number;
+
+            int x = 10;
+            x = x * 7;
+            x = x/3;
+            Console.WriteLine(x);
+
+
+            /*
+            // More runtime and logic errors:
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine()!.Trim();
+            int howManyFit = 100 / name.Length;
+            Console.WriteLine($"{name} has {name.Length} letters and can fit {howManyFit} times in 100 characters.");
+            Console.WriteLine(name + " can fit " + howManyFit + ".");
+            */
         }
     }
 }
---
+++ IfStatementDemo/Program.cs
@@ -0,0 +1,85 @@
+﻿
+// Erin Cascioli
+// 9/20/24
+// Demo: If Statements and If-Else Statements
+
+namespace IfStatementDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ----------------------------------------------------------------
+            // Variable block
+            // ----------------------------------------------------------------
+            const int correctNumber = 7;
+
+
+            // ----------------------------------------------------------------
+            // Get user data
+            // ----------------------------------------------------------------
+            // Grab user's guessed number and their chosen color
+            Console.Write("Guess a whole number between 1 and 10: ");
+
+            // Parse their input to an integer
+            // Can do it in two lines like this:
+            //string userInput = Console.ReadLine()!;
+            //int userInputAsInt = int.Parse(userInput);
+            // Or in one line like this:
+            int userNumber = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Choose the best color: red, green or blue? ");
+            string userColor = Console.ReadLine()!.ToUpper().Trim();
+
+
+            // ----------------------------------------------------------------
+            // Change console color
+            // ----------------------------------------------------------------
+            // Change color based on user's choice
+            if (userColor == "RED")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Gray;
+            }
+            else if (userColor == "GREEN")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (userColor == "BLUE")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+
+
+            // ----------------------------------------------------------------
+            // Display guess results
+            // ----------------------------------------------------------------
+            // Display results of their guess
+            if (userNumber == correctNumber)
+            {
+                Console.WriteLine("Yay you guessed right!");
+            }
+            if (userNumber < correctNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            if (userNumber > correctNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            /*
+           // You could do this as well:
+           bool guessedCorrectNumber = (userNumber == 7);
+           if (guessedCorrectNumber)       // if (guessedCorrectNumber == true)
+           {
+               Console.WriteLine("Yay you guessed right!");
+           }
+           */
+        }
+    }
+}
