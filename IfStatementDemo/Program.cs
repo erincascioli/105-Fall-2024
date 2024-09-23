b7a7e969db380cdc415151a20601530cca8add7d
erincascioli <edcigm@rit.edu>
2024-09-23 16:30:37 -0400

    Pushing if statement demo
    Includes if statements, if/else statements, if/elseif/else statements, along with a an example of a nested conditional.

--- IfStatementDemo/Program.cs
+++ IfStatementDemo/Program.cs
@@ -12,12 +12,22 @@ namespace IfStatementDemo
             // ----------------------------------------------------------------
             // Variable block
             // ----------------------------------------------------------------
+            // These variables are within scope throughout all of the Main method.
             const int correctNumber = 7;
+            int health = -3;
 
 
             // ----------------------------------------------------------------
             // Get user data
             // ----------------------------------------------------------------
+
+            // PSEUDOCODE:
+            // Prompt for input of integer ranged 1 - 10
+            // Get user's data
+            // Parse to integer
+            // Prompt for color: red, green, blue
+            // Get user's data as string
+
             // Grab user's guessed number and their chosen color
             Console.Write("Guess a whole number between 1 and 10: ");
 
@@ -34,8 +44,24 @@ namespace IfStatementDemo
 
             // ----------------------------------------------------------------
             // Change console color
-            // ----------------------------------------------------------------
-            // Change color based on user's choice
+            // ---------------------------------------------------------------
+            // This code has 3 distinct possibilities with a "catch-all" case
+            //   handled by the else statement. The user could type red, or green,
+            //   or blue, or ANYTHING ELSE at all. The else handles the "anything else"
+            //   case. 
+
+            // PSEUDOCODE:
+            // IF chose red
+            //      Change text to red
+            // IF chose blue
+            //      Change text to blue
+            // IF chose green
+            //      Change text to green
+            // ELSE types anything else
+            //      Display "Invalid" message
+            //      Change text to yellow  
+
+            // Change color based on user's valid choices of red, green, or blue
             if (userColor == "RED")
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -49,37 +75,107 @@ namespace IfStatementDemo
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
             }
+            // User typed an invalid option --> Change text to yellow. 
             else
             {
+                // Invalid option! Let the user know, don't make them guess. 
+                Console.WriteLine($"I do not recognize {userColor}.");
                 Console.ForegroundColor = ConsoleColor.Yellow;
             }
 
 
             // ----------------------------------------------------------------
-            // Display guess results
+            // Display guess results with PSEUDOCODE
             // ----------------------------------------------------------------
-            // Display results of their guess
+
+            // PSEUDOCODE EXAMPLE:
+            // Pseudocode is a way to describe the steps of an algorithm or program
+            // using a natural language like English, mixed with programming language syntax.
+            // This pre-planning can save hours of time debugging!
+            
+            // IF num is correct
+            //      Display congrats message
+            // IF number is smaller than correct
+            //      IF number is less than 1
+            //          Display "Invalid"
+            //      ELSE number is 1, 2, 3, 4, 5, 6
+            //          Display "Too low"
+            // IF number is greater than correct
+            //      IF number is more than 10
+            //          Display "Invalid"
+            //      ELSE number is 8, 9, 10
+            //          Display "Too high"  
+
+
+            // Correct guess!
+            // ** See below for another way of writing this using a Boolean variable
             if (userNumber == correctNumber)
             {
                 Console.WriteLine("Yay you guessed right!");
             }
-            if (userNumber < correctNumber)
+            // Guess is smaller
+            else if (userNumber < correctNumber)
             {
-                Console.WriteLine("Too low!");
+                // 0 or negative values
+                if(userNumber < 1)          // Or you could say (userNumber <= 0)
+                {
+                    Console.WriteLine("That value is invalid. Choose a number between 1 and 10.");
+                }
+                // 1, 2, 3, 4, 5, 6
+                else
+                {
+                    Console.WriteLine("Too low!");
+                }
             }
-            if (userNumber > correctNumber)
+            // Guess is larger
+            else                            // Or you could say if (userNumber > correctNumber)
             {
-                Console.WriteLine("Too high!");
+                // 11 or higher
+                if (userNumber > 10)        // Or you could say (userNumber <= 11>)
+                {
+                    Console.WriteLine("That value is invalid. Choose a number between 1 and 10.");
+                }
+                // 8, 9, 10
+                else
+                {
+                    Console.WriteLine("Too high!");
+                }
             }
 
-            /*
-           // You could do this as well:
-           bool guessedCorrectNumber = (userNumber == 7);
-           if (guessedCorrectNumber)       // if (guessedCorrectNumber == true)
-           {
-               Console.WriteLine("Yay you guessed right!");
-           }
-           */
+
+            // ----------------------------------------------------------------
+            // Scope in your programs!
+            // ----------------------------------------------------------------
+            // Variable declared within a code block exist only within that
+            //   code block and any underneath/nested within.  
+            // This output variable exists and is accessible within the Main
+            //   method and in any nested code blocks here in Main, meaning it's 
+            //   accessible in any if or else statements here. 
+
+            // In this code block, the string variable output only exists within
+            //   the if or else block. By the time Main attempts to access the variable
+            //   in the C.WL line, the variable no longer exists.
+            //if (health <= 0)
+            //{
+            //    string output = "dead";
+            //}
+            //else
+            //{
+            //    string output = "alive";
+            //}
+
+            //Console.WriteLine("The character is " + output);
+
+
+            // However, in this case the variable is declared within Main, so it's
+            //   entirely accessible in the if statement and all of Main.
+            string output = "alive";
+            if (health <= 0)
+            {
+                output = "dead";
+            }
+
+            Console.WriteLine("The character is " + output);
         }
     }
 }
