4b207f60085e08c1c7fddec0291bab85cea4b89c
erincascioli <edcigm@rit.edu>
2024-10-04 14:45:49 -0400

    Pushing arrays demo

---
+++ Arrays_Demo/Program.cs
@@ -0,0 +1,87 @@
+﻿
+// Erin Cascioli
+// 10/4/24
+// Demo: Arrays (1-dimensional)
+
+namespace Arrays_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ----------------------------------------------------------------
+            // Arrays 1-dimensional
+            // ----------------------------------------------------------------
+            // Arrays hold consecutive data in memory. 
+            // They are indexed by an integral value, starting at 1 through one less
+            //   than the array's length. 
+
+            // *** Declare an array ***
+            // This array is declared only and not initialized because we don't know
+            //   how many values the array should contain yet. The user provides that
+            //   information.
+            string[] userProvidedNames;
+
+            // *** Declare and initialize an array ***
+            // When you know all the data, you can initialize the array with data
+            //   immediately. 
+            int[] birthdaysInMonths = new int[12];
+            birthdaysInMonths[0] = 2;       // Jan
+            birthdaysInMonths[1] = 2;       // Feb
+            birthdaysInMonths[2] = 0;       // Mar
+            birthdaysInMonths[3] = 1;       // Apr
+            birthdaysInMonths[4] = 1;       // May
+            birthdaysInMonths[5] = 2;       // Jun
+            birthdaysInMonths[6] = 2;       // Jul
+            birthdaysInMonths[7] = 1;       // Aug
+            birthdaysInMonths[8] = 1;       // Sep
+            birthdaysInMonths[9] = 1;       // Oct
+            birthdaysInMonths[10] = 2;      // Nov
+            birthdaysInMonths[11] = 3;      // Dec
+
+            // *** "Literal" array ***
+            // Great option when you know the data to begin with!
+            // Literal arrays eliminate the need to use a separate line for each element
+            //   when initializing with data.
+            // However, literal arrays cannot be used for user-provided information
+            //   or for any type of iteration necessary for initial data providing. 
+            int[] birthdaysInMonthsLiteral = { 2, 2, 0, 1, 1, 2, 2, 1, 1, 1, 2, 3 };
+
+            // Literal string array
+            string[] characterNames = {"Banana McGillicuddy", "Spooner McForkson", "Jeebus"};
+
+            // *** Change value in array ***
+            // Values inside arrays can be changed with the assignment variable. 
+            characterNames[1] = "AAAAAAAAAA";
+
+            // *** Getting data from a user ***
+            // Prompt a user for a number of names:
+            Console.Write("How many names? ");
+            int numberOfNames = int.Parse(Console.ReadLine()!);
+
+            // Use that parsed value to initialize the array with a size
+            userProvidedNames = new string[numberOfNames];
+
+            // Get all information from the user
+            for(int i = 0; i < numberOfNames; i++)
+            {
+                Console.Write("Enter a name:");
+                userProvidedNames[i] = Console.ReadLine()!;
+            }
+
+            // *** Printing array data ***
+            Console.WriteLine("All character names: ");
+
+            // Using the name of the array will provide you with the TYPE of array
+            // This statement will *not* print each element of the array. 
+            Console.WriteLine(characterNames);
+
+            // Instead, array data must be iterated through to access each element. 
+            for(int i = 0; i < characterNames.Length; i++)
+            {
+                // Print the data at this element
+                Console.WriteLine(" --> " + characterNames[i]);
+            }
+        }
+    }
+}
---
+++ ForLoops_Demo/Program.cs
@@ -0,0 +1,286 @@
+﻿
+
+
+
+namespace ForLoops_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // 1 - 10 in WHILE loop
+            int counter = 1;
+            while (counter <= 10)
+            {
+                Console.Write(counter + " ");
+                counter++;
+            }
+
+            Console.WriteLine("\n********************");
+
+            // 1 - 10 in FOR loop
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n********************");
+
+            // 10 - 1 in FOR loop
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n********************");
+
+            // 200 - 300 in multiples of 5 
+            for (int i = 200; i <= 300; i += 5)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n********************");
+
+            // 300 to 200 backwards
+            for (int a = 300; a >= 200; a -= 5)
+            {
+                Console.Write(a + " ");
+            }
+
+            // Iterate through a string via its indices
+            string someWord = "dfighsrgkjDBKJvzfdxbgc";
+            //for (int i = 0; i <= someWord.Length - 1; i++)
+            for (int i = 0; i <= someWord.Length; i++)
+            {
+                Console.WriteLine(someWord[i]);
+            }
+            */
+
+
+            // BREAK AND CONTINUE
+            // 1 2 3
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+
+                if (i == 3)
+                {
+                    break;
+                }
+            }
+
+            // 1 2 4 5 6 7 8 9 10
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+
+                Console.Write(i + " ");
+            }
+
+
+
+            // What does this output?
+
+            for(int row = 1; row < 3; row++)
+            {
+                for(int col = 1; col < 4; col++)
+                {
+                    Console.WriteLine($"{row}, {col}");
+                }
+            }
+
+
+
+
+
+
+
+            #region MINIMIZE THIS
+
+
+            // PSEUDOCODE:
+            //
+            // Prompt user for a number 1+
+            // Parse to integer
+            // WHILE number is invalid (0 or less)
+            //     Re-prompt for data
+            //     Parse to integer
+            // WHILE counter is less than user's value
+            //     Print counter
+            //
+            // CODE:
+
+            //int userNumber = 0;
+            //bool firstPrompt = true;
+
+            /*
+            do
+            {
+                if(firstPrompt)
+                {
+                    Console.Write("Enter: ");
+                    userNumber = int.Parse(Console.ReadLine()!);
+                    firstPrompt = false;
+                }
+                else
+                {
+                    Console.Write("Invalid. Enter again: ");
+                    userNumber = int.Parse(Console.ReadLine()!);
+                }
+            }
+            while (userNumber < 1);
+            */
+
+
+            /*
+            Console.Write("Enter: ");
+           
+            do
+            {
+                userNumber = int.Parse(Console.ReadLine()!);
+
+                if(userNumber < 1)
+                {
+                    Console.Write("Invalid. Enter again: ");
+                }
+            }
+            while (userNumber < 1);
+            */
+
+
+
+            /*
+            int counter = 1;
+            while(counter <= userNumber)
+            {
+                Console.Write(counter + " ");
+                counter++;
+            }
+            */
+
+
+
+
+            /*
+            // 1 2 3 4 5
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+
+                if(i == 5)
+                {
+                    break;
+                }
+            }
+
+            // 1 2 3 4 5 6 7 8 9 10
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+
+                Console.Write(i + " ");
+            }
+            */
+
+
+
+            /*
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n********************");
+
+            for(int i = 200; i <= 300; i += 5)
+            {
+                Console.Write(i + " ");
+            }
+
+
+            Console.WriteLine("\n********************");
+
+            for (int a = 300; a >= 200; a -= 5)
+            {
+                Console.Write(a + " ");
+            }
+
+            string someWord = "dfighsrgkjDBKJvzfdxbgc";
+            for(int i = 0; i <= someWord.Length - 1; i++)
+            {
+                Console.WriteLine(someWord[i]);
+            }
+            */
+
+
+
+
+            // PSEUDOCODE:
+            //
+            // Prompt user for a number 1+
+            // Parse to integer
+            // WHILE number is invalid (0 or less)
+            //     Re-prompt for data
+            //     Parse to integer
+            // WHILE counter is less than user's value
+            //     Print counter
+            //
+            //
+            // CODE:
+            /*
+            Console.Write("Enter: ");
+            int userNumber = int.Parse(Console.ReadLine()!);
+
+            while(userNumber < 1)
+            {
+                Console.Write("Invalid. Enter again: ");
+                userNumber = int.Parse(Console.ReadLine()!);
+            }
+
+            int counter = 1;
+            while(counter <= userNumber)
+            {
+                Console.Write(counter + " ");
+                counter++;
+            }
+            */
+
+            // ------------------------------------------------------------------
+            /*
+            int userNumber = 9999999;
+
+            do
+            {
+                if(userNumber < 1)
+                {
+                    Console.Write("Invalid. Enter again: ");
+                    userNumber = int.Parse(Console.ReadLine()!);
+                }
+                else
+                {
+                    Console.Write("Enter: ");
+                    userNumber = int.Parse(Console.ReadLine()!);
+                }
+            }
+            while (userNumber < 1);
+
+            int counter = 1;
+            while (counter <= userNumber)
+            {
+                Console.Write(counter + " ");
+                counter++;
+            }
+            */
+
+            #endregion
+        }
+    }
+}
